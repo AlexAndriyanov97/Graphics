9d170cd65373b80035d51873dc656d445839e141
Alex <alexandriyanov97@gmail.com>
2019-05-25 00:45:00 +0700

    add Segment, Separator

---
+++ src/main/java/ru/nsu/fit/andriyanov/graphics/model/Separator.java
@@ -0,0 +1,32 @@
+package main.java.ru.nsu.fit.andriyanov.graphics.model;
+
+public class Separator {
+
+    private final double from;
+    private final int count;
+
+    private double value;
+    private double step;
+    private int c = 0;
+
+    public Separator(double from, double to, int count) {
+        this.from = from;
+        this.count = count;
+
+        value = from;
+        step = (to - from) / (count - 1);
+    }
+
+    public double next() {
+        double next = value;
+
+        if (count == ++c) {
+            c = 0;
+            value = from;
+        } else {
+            value += step;
+        }
+
+        return next;
+    }
+}
