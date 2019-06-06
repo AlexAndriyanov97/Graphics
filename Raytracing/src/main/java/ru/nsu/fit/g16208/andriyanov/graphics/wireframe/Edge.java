d851cd953ff2443908af43bd46ad9008f6e24ce6
Alex <alexandriyanov97@gmail.com>
2019-06-06 19:31:47 +0700

    add basic functional

---
+++ Raytracing/src/main/java/ru/nsu/fit/g16208/andriyanov/graphics/wireframe/Edge.java
@@ -0,0 +1,32 @@
+package ru.nsu.fit.g16208.andriyanov.graphics.model;
+
+import java.awt.*;
+
+public class Edge {
+    private Vector[] points = new Vector[2];
+    private Color color = Color.WHITE;
+
+    public Edge(Vector p1, Vector p2, Color color) {
+        points[0] = p1;
+        points[1] = p2;
+
+        this.color = color;
+    }
+
+    public Edge(Vector p1, Vector p2) {
+        points[0] = p1;
+        points[1] = p2;
+    }
+
+    public Vector[] getPoints() {
+        return points;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
